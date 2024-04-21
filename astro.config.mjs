592389828ea86c6acf32bf5ae945af91f3e6bb0a
Thian Lopez <thian.lopez@parallelpublicworks.com>
2024-04-20 23:34:38 -0500

    Add analitycs script in body

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'  
+import partytown from '@astrojs/partytown'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vntotxt.com",
   integrations: [tailwind({ applyBaseStyles: false }),
-		partytown({
-			config: {
-			  forward: ["dataLayer.push"],
-			},
-		}),],
+  partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }),],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
