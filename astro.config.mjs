e3a9e8fa31159536b76370e713caf3e2a9052c03
Thian Lopez <thian.lopez@parallelpublicworks.com>
2024-04-21 00:51:10 -0500

    Tags partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import partytown from '@astrojs/partytown'
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vntotxt.com",
-  integrations: [tailwind({ applyBaseStyles: false }),
-  partytown({
-    config: {
-      forward: ["dataLayer.push"],
-    },
-  }),],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"]
+      },
+    }),
+  ],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
