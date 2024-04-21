5d85b428bae88fe015f65e3763f3a61036356308
Thian Lopez <thian.lopez@parallelpublicworks.com>
2024-04-20 22:18:11 -0500

    Added GA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown'  
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://vntotxt.com",
-  integrations: [tailwind({ applyBaseStyles: false })],
+  integrations: [tailwind({ applyBaseStyles: false }),
+		partytown({
+			config: {
+			  forward: ["dataLayer.push"],
+			},
+		}),],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
