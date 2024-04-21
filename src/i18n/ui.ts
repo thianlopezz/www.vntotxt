17aae5de5036de09b30d5da710c62bbaf86de780
Thian Lopez <thian.lopez@parallelpublicworks.com>
2024-04-20 23:24:39 -0500

    Internationalized text

---
+++ src/i18n/ui.ts
@@ -0,0 +1,99 @@
+export const languages = {
+  en: "English",
+  es: "Spanish",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.howItWorks": "How it works",
+    "nav.features": "Features",
+    "nav.pricing": "Pricing",
+    "home.mainTitle.1": "Transcribe your",
+    "home.mainTitle.2": "voice notes",
+    "home.mainTitle.3": "now",
+    "home.mainDescription":
+      "Instantly convert your WhatsApp voice messages into text with a single forward.",
+    "home.mainCallToAction": "Get started!",
+    "home.howItWorks.title": "How it works",
+    "home.howItWorks.subtitle": "As simple as",
+    "home.howItWorks.step-1.altImage": "Save WhatsApp contact",
+    "home.howItWorks.step-1.title": "Save our contact",
+    "home.howItWorks.step-1.body": "Keep us close in your WhatsApp contacts.",
+    "home.howItWorks.step-2.altImage": "Forward voice note",
+    "home.howItWorks.step-2.title": "Forward us a voice note",
+    "home.howItWorks.step-2.body": "Or just DM us an audio message.",
+    "home.howItWorks.step-3.altImage": "Wait for transcribed audio",
+    "home.howItWorks.step-3.title": "Wait for it",
+    "home.howItWorks.step-3.body":
+      "We'll send you back your audio transcribed in few seconds.",
+    "home.features.title.1": "Leverage your voice notes",
+    "home.features.title.2": "transcriptions",
+    "home.features.title.3": "",
+    "home.features.item.1.title": "For Everyone, Everywhere",
+    "home.features.item.1.description":
+      "Noisy room? Hard to hear? VNTOTXT changes voice notes to text, so you can read what's being said.",
+    "home.features.item.2.title": "Listen with Your Eyes",
+    "home.features.item.2.description":
+      "VNTOTXT turns your voice notes into text, making sure you never miss a word. Perfect for catching every detail!",
+    "home.features.item.3.title": "Find What You Need",
+    "home.features.item.3.description":
+      "With VNTOTXT, your words turn into text, so you can find exactly what you're looking for, fast!",
+    "home.features.item.4.title": "From Spoken to Written",
+    "home.features.item.4.description":
+      "Got a great idea but no hands free? Just speak! VNTOTXT captures your voice notes and turns them into text.",
+    "home.features.callToAction": "Start forwarding",
+    "home.pricing.title": "Pricing",
+    "home.pricing.subtitle": "Our fees",
+    "footer.privacy": "Privacy",
+    "footer.contact": "Contact",
+    "footer.language": "Español",
+    "footer.rights": "All right reserved by",
+  },
+  es: {
+    "nav.howItWorks": "Como funciona",
+    "nav.features": "Funcionalidades",
+    "nav.pricing": "Precios",
+    "home.mainTitle.1": "Transcribe tus",
+    "home.mainTitle.2": "notas de voz",
+    "home.mainTitle.3": "ahora",
+    "home.mainDescription":
+      "Convierte tus mensajes de voz de WhatsApp en texto instantáneamente con solo reenviarlos.",
+    "home.mainCallToAction": "Empezar ya!",
+    "home.howItWorks.title": "Cómo funciona",
+    "home.howItWorks.subtitle": "Tan simple como",
+    "home.howItWorks.step-1.altImage": "Guardar nuestro contacto de WhatsApp",
+    "home.howItWorks.step-1.title": "Guarda nuestro contacto de WhatsApp",
+    "home.howItWorks.step-1.body": "Mantennos cerca en tus contactos de WhatsApp.",
+    "home.howItWorks.step-2.altImage": "Reenviar nota de voz",
+    "home.howItWorks.step-2.title": "Reenvíanos una nota de voz",
+    "home.howItWorks.step-2.body": "O simplemente envíanos un mensaje de audio por DM.",
+    "home.howItWorks.step-3.altImage": "Espera tu audio transcrito",
+    "home.howItWorks.step-3.title": "Espera tu audio transcrito",
+    "home.howItWorks.step-3.body":
+      "Te devolveremos tu audio transcrito en unos segundos.",
+    "home.features.title.1": "Aprovecha las",
+    "home.features.title.2": "transcripciones",
+    "home.features.title.3": "de tus notas de voz",
+    "home.features.item.1.title": "Para todos, en todas partes",
+    "home.features.item.1.description":
+      "¿Sala ruidosa? ¿Difícil de escuchar? VNTOTXT convierte las notas de voz en texto, para que puedas leer lo que se dice.",
+    "home.features.item.2.title": "Escucha con tus ojos",
+    "home.features.item.2.description":
+      "VNTOTXT convierte tus notas de voz en texto, asegurando que no te pierdas ni una palabra. ¡Perfecto para captar cada detalle!",
+    "home.features.item.3.title": "Encuentra lo que necesitas",
+    "home.features.item.3.description":
+      "Con VNTOTXT, tus palabras se convierten en texto, así puedes encontrar exactamente lo que buscas, ¡rápido!",
+    "home.features.item.4.title": "De lo hablado a lo escrito",
+    "home.features.item.4.description":
+      "¿Tienes una gran idea pero sin manos libres? ¡Solo habla! VNTOTXT captura tus notas de voz y las convierte en texto.",
+    "home.features.callToAction": "Empieza a reenviar tus Vn's",
+    "home.pricing.title": "Precios",
+    "home.pricing.subtitle": "Nuestras tarifas",
+    "footer.privacy": "Privacidad",
+    "footer.contact": "Contacto",
+    "footer.language": "English",
+    "footer.rights": "Todos los derechos reservados",
+  },
+} as const;
