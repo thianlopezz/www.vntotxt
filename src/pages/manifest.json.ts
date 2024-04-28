144f9d1f10e2a973cf8fa2e507b1ae2b566dea92
thianlopezz <thianlopezz@gmail.com>
2024-04-28 08:30:49 +0000

    Favico

---
+++ src/pages/manifest.json.ts
@@ -0,0 +1,38 @@
+import type { APIRoute } from 'astro'
+import { getImage } from 'astro:assets'
+import favicon from '../../public/logo.png'
+import { getLangFromUrl, useTranslations } from "../i18n/utils";
+
+const t = useTranslations("en");
+
+const faviconPngSizes = [192, 512]
+
+export const GET: APIRoute = async () => {
+  console.log('aaass');
+  const icons = await Promise.all(
+    faviconPngSizes.map(async (size) => {
+      const image = await getImage({
+        src: favicon,
+        width: size,
+        height: size,
+        format: 'png'
+      })
+      return {
+        src: image.src,
+        type: `image/${image.options.format}`,
+        sizes: `${image.options.width}x${image.options.height}`
+      }
+    })
+  )
+
+  const manifest = {
+    name: t('page.title'),
+    description: t('page.description'),
+    start_url: '/',
+    display: 'standalone',
+    id: 'vntotxt',
+    icons
+  }
+
+  return new Response(JSON.stringify(manifest))
+}
