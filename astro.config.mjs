9555d1aa60abe7d34f28f78db6ab21b73384a85a
thianlopezz <thianlopezz@gmail.com>
2024-05-23 05:00:05 +0000

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,25 +2,24 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://vntotxt.com",
   trailingSlash: 'never',
-  integrations: [
-    tailwind({
-      applyBaseStyles: false,
-    }),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [tailwind({
+    applyBaseStyles: false
+  }), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()],
   i18n: {
     locales: ["en", "es"],
     defaultLocale: "en",
     prefixDefaultLocale: true,
-    redirectToDefaultLocale: true,
+    redirectToDefaultLocale: true
   },
   vite: {
     define: {
