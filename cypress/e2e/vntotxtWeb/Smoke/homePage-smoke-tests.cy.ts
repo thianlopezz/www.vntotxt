111608c61e59e16633967b6173aca55148fbbc45
y804327 <138408901+lyagualAYA@users.noreply.github.com>
2024-05-24 12:07:23 -0500

    update tests cypress

--- cypress/e2e/vntotxtWeb/Smoke/homePage-smoke-tests.cy.ts
+++ cypress/e2e/vntotxtWeb/Smoke/homePage-smoke-tests.cy.ts
@@ -1,8 +1,16 @@
+let fixtureData: { [x: string]: any; };
 describe('Web Page Home smoke tests', () => {
+  before(() => {
+    cy.fixture('vntotxt').then((data) => {
+      fixtureData = data;
+    });
+})
   beforeEach(() => {
     cy.visit('')
 })
-  it('Visit web page and no console errors', () => {
+  it('Verify that when visiting web page and no console errors appears', () => {
+    cy.get('#hero-section').should('be.visible');
+    cy.get('#phone-section').should('be.visible');
     // Check for console errors
     cy.window().then((win) => {
       cy.stub(win.console, 'error').as('consoleError');
@@ -11,19 +19,41 @@ describe('Web Page Home smoke tests', () => {
     cy.get('@consoleError').should('not.be.called');
   })
 
-
-  it('Click on How it Works option should redirect to How it works section', () => {
+  it('Verify that when clicking on How it Works option, it redirects to How it works section in standard resolution', () => {
     // Set the viewport to laptop
     cy.viewport('macbook-15')
-    cy.get('a[href="#how-it-works"]').should('be.visible').and('contain', 'How it works').click();
-    cy.get('#how-it-works').should('be.visible');
-    // Set the viewport to mobile
-    cy.viewport('iphone-8')
-    cy.get('a[href="#how-it-works"]').should('be.visible').and('contain', 'How it works').click();
-    cy.get('#how-it-works').should('be.visible');
-    // Set the viewport to ipad
-    cy.viewport('ipad-2')
-    cy.get('a[href="#how-it-works"]').should('be.visible').and('contain', 'How it works').click();
+    cy.get('#how-it-works-link').should('be.visible').and('contain', 'How it works').click();
     cy.get('#how-it-works').should('be.visible');
   })
+
+  it('Verify that when clicking on How it Works option, it redirects to How it works section in mobile resolution', () => {
+    // Set the viewport to mobile
+    cy.viewport('iphone-8')
+    cy.get('.dropdown').should('be.visible').click();
+    cy.get('#how-it-works-mobile-link').should('be.visible').and('contain', 'How it works').click();
+    cy.get('#how-it-works').should('be.visible');
+  })
+
+  it('Verify that when clicking on How it Works option, it redirects to How it works section in tablet resolution', () => {
+    // Set the viewport to mobile
+    cy.viewport('ipad-2')
+    cy.get('.dropdown').should('be.visible').click();
+    cy.get('#how-it-works-mobile-link').should('be.visible').and('contain', 'How it works').click();
+    cy.get('#how-it-works').should('be.visible');
+  })
+
+/*Current cypress bug> https://github.com/cypress-io/cypress/issues/29196
+it.only('Verify that when clicking on Get Started button, it redirects to Whatsapp continue to chat option', () => {
+    cy.contains('a', 'Get started!')
+          .invoke('removeAttr', 'target')
+          .then($a => {
+              const href = $a.prop('href');
+              // Click the link and visit the URL manually
+              cy.wrap($a).click();
+              cy.visit(href);
+          });
+
+        // Verify the new URL includes the phone number from the fixture
+        cy.url().should('include', `https://api.whatsapp.com/send?phone=${fixtureData['bot-phone']}`);
+  })*/
 })
