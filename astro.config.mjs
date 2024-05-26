0fcbeeff0ea1188522d8e6f2451e469f466d916c
thianlopezz <thianlopezz@gmail.com>
2024-05-26 18:18:10 +0000

    Fix subscription success to 404 bug

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,6 @@ import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
 export default defineConfig({
   site: "https://vntotxt.com",
-  trailingSlash: 'never',
   integrations: [tailwind({
     applyBaseStyles: false
   }), partytown({
